7804f533eef45676967bd9316796b84267e11119
damionbrown <dbrown0591>
2018-03-23 20:04:45 -0500

    .gitignore change

---
+++ src/main/java/com/codeup/michero/MicheroApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.michero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicheroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codeup/michero/MicheroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.michero;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicheroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
