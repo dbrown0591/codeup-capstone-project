a9a6b3f6a5149e6b6e399398176b60990b85028f
damionbrown <dbrown0591>
2018-05-16 19:20:49 -0500

    Delete account bug

--- src/main/java/com/codeup/michero/controller/UserController.java
+++ src/main/java/com/codeup/michero/controller/UserController.java
@@ -2,11 +2,13 @@ package com.codeup.michero.controller;
 
 import com.codeup.michero.daos.UsersRepository;
 import com.codeup.michero.models.User;
+import com.codeup.michero.services.UserDetailsLoader;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -36,5 +38,11 @@ public class UserController {
         //needed a place to add a profile pic on the sign up page.
         usersRepository.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/users/delete")
+    public String delete(){
+
+        return "users/delete-account";
     }
 }
