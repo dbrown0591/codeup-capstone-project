d63bde0d0030e91f73993614e6164b3696e00251
damionbrown <dbrown0591>
2018-06-20 14:02:04 -0500

    Prepare app for deployment

--- src/main/java/com/codeup/michero/MicheroApplication.java
+++ src/main/java/com/codeup/michero/MicheroApplication.java
@@ -2,11 +2,17 @@ package com.codeup.michero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MicheroApplication {
+public class MicheroApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicheroApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MicheroApplication.class);
 	}
 }
